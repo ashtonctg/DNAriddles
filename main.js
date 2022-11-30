8039e74d73f1dfd80a303cd3068138e9b419c272
ashtonctg <ash.trotmangrant@gmail.com>
2022-11-29 22:48:29 -0800

    made rudimentary website

---
+++ main.js
@@ -0,0 +1,7 @@
+var hi = 'cool'
+
+console.log('hello marc', hi)
+const button = document.querySelector('button')
+button.addEventListener('click', () => { console.log('hi Ash!!!') })
+const p = document.querySelector('p')
+p.addEventListener('click', () => { console.log('hello world')})
